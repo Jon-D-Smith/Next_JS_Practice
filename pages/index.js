cd39d7ffc68b9d89fb644ab496eb3dcb73069daf
Jon Smith <aardvarkjon94@yahoo.com>
2021-06-27 15:13:31 -0500

    Practiced with Linking in Next

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,17 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Navbar from '../comps/Navbar'
 import Footer from '../comps/Footer'
+import Link from 'next/link'
 export default function Home() {
   return (
    <div>
      <Navbar />
      <h1>Hello World</h1>
+     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam corrupti illum ipsum itaque beatae amet dicta modi omnis neque? Dolores illo molestias itaque voluptate molestiae sequi reiciendis beatae ducimus minima!</p>
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem in aliquid, dolores earum expedita pariatur accusantium! Voluptas repudiandae incidunt exercitationem dolores? Consectetur quis, error esse praesentium ea tempora ratione tempore.</p>
+     <Link href="/ninjas">
+     <a>See Nina Listing</a>
+     </Link>
      <Footer />
    </div>
   )
