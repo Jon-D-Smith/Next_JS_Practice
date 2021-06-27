d1acc0d857496678662fed592b3c21731196733e
Jon Smith <aardvarkjon94@yahoo.com>
2021-06-27 15:02:02 -0500

    Setup basic structure and a couple of pages

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+const About = () => {
+    return ( 
+        <div>
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta minima laudantium velit accusamus, maxime illo quisquam distinctio at obcaecati recusandae cupiditate repellat non fugiat ipsa, commodi consequatur! In, ad doloremque.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda ducimus, adipisci esse suscipit dolores placeat ipsum, repellat ab quasi sint aspernatur accusamus recusandae iste tempora distinctio vero non minima! Eos.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis rem iste dignissimos illum! Doloremque iusto cumque saepe laborum! Libero esse, quos similique quia facilis sunt molestiae qui nobis nostrum natus.</p>
+        </div>
+     );
+}
+ 
+export default About;
