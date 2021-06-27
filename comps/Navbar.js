024d4dff2b4394ce74efce43c6b15132187fc0b2
Jon Smith <aardvarkjon94@yahoo.com>
2021-06-27 15:41:19 -0500

    Added a 404 page

--- comps/Navbar.js
+++ comps/Navbar.js
@@ -5,10 +5,11 @@ const Navbar = () => {
             <div className="logo">
                 <h1>Ninja List</h1>
             </div>
-            
-            <Link href="/"><a>Home</a></Link>
-            <Link href="/about"><a>About</a></Link>
-            <Link href="/ninjas"><a>Ninja Listing</a></Link>
+            <ul>
+            <Link href="/"><li>Home</li></Link>
+            <Link href="/about"><li>About</li></Link>
+            <Link href="/ninjas"><li>Ninja Listing</li></Link>
+            </ul>
         </nav>
      );
 }
--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ import Footer from '../comps/Footer'
 import Link from 'next/link'
 export default function Home() {
   return (
-   <div>
+   <div className={styles.container}>
      <h1>Hello World</h1>
      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam corrupti illum ipsum itaque beatae amet dicta modi omnis neque? Dolores illo molestias itaque voluptate molestiae sequi reiciendis beatae ducimus minima!</p>
      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem in aliquid, dolores earum expedita pariatur accusantium! Voluptas repudiandae incidunt exercitationem dolores? Consectetur quis, error esse praesentium ea tempora ratione tempore.</p>
