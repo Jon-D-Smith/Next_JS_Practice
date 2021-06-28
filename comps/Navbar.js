d3a5c01062dbeae10899c9e6e8123fc02189d6df
Jon Smith <aardvarkjon94@yahoo.com>
2021-06-28 13:02:30 -0500

    Fixed NavBar

--- comps/Navbar.js
+++ comps/Navbar.js
@@ -3,12 +3,11 @@ const Navbar = () => {
     return ( 
         <nav>
             <div className="logo">
-                <h1>Ninja List</h1>
+                <h1>Practice Site</h1>
             </div>
             <ul>
             <Link href="/"><li>Home</li></Link>
             <Link href="/about"><li>About</li></Link>
-            <Link href="/ninjas"><li>Ninja Listing</li></Link>
             </ul>
         </nav>
      );
