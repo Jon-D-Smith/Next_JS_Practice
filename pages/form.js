6404ea408e76b7d4fcf9840a2d60d74c2de1382c
Jon Smith <aardvarkjon94@yahoo.com>
2021-06-30 21:52:08 -0500

    Got rid of button element

--- pages/form.js
+++ pages/form.js
@@ -5,25 +5,16 @@ import Buttons from '../comps/Buttons';
 const Form = () => {
     const [adjective, setAdjective] = useState()
 
-    const [story, setStory] = useState(`There once was a boy with a ${adjective} balloon`)
-    const [isVisible, setIsVisible] = useState(false)
-
     const handleAdjectiveChange =(e) => {
         e.preventDefault()
         setAdjective(e.target.value)
     }
 
-    const handleSubmit =(e) => {
-        e.preventDefault()
-        setIsVisible(true)
-    }
     return ( 
         <>
     <form noValidate autoComplete="off">
-      <TextField label="Adjective" variant="filled"  value={adjective} onChange={handleAdjectiveChange} />
-      <Buttons text='Generate Story' variant="contained" color="primary" handleClick={handleSubmit} />
+      <TextField label="Adjective" variant="filled" name="adjective"  value={adjective} onChange={handleAdjectiveChange} />
     </form>
-    {isVisible && <p>{story}</p>}
     </>
      );
 }
