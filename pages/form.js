e7e2d372c32fd4ff636f1bc64442f57b43a7f533
Jon Smith <aardvarkjon94@yahoo.com>
2021-06-30 21:51:35 -0500

    Got rid of additional form elements

--- pages/form.js
+++ pages/form.js
@@ -5,16 +5,25 @@ import Buttons from '../comps/Buttons';
 const Form = () => {
     const [adjective, setAdjective] = useState()
 
+    const [story, setStory] = useState(`There once was a boy with a ${adjective} balloon`)
+    const [isVisible, setIsVisible] = useState(false)
+
     const handleAdjectiveChange =(e) => {
         e.preventDefault()
         setAdjective(e.target.value)
+    }
+
+    const handleSubmit =(e) => {
+        e.preventDefault()
+        setIsVisible(true)
     }
     return ( 
         <>
     <form noValidate autoComplete="off">
       <TextField label="Adjective" variant="filled"  value={adjective} onChange={handleAdjectiveChange} />
+      <Buttons text='Generate Story' variant="contained" color="primary" handleClick={handleSubmit} />
     </form>
-    {adjective}
+    {isVisible && <p>{story}</p>}
     </>
      );
 }
