4d4b73b8007ccabefe37f0ba197281d5c79f2099
Jon Smith <aardvarkjon94@yahoo.com>
2021-06-30 22:02:46 -0500

    Got the basics of an adlib setup

--- pages/form.js
+++ pages/form.js
@@ -3,18 +3,28 @@ import { useState } from 'react';
 import Buttons from '../comps/Buttons';
 
 const Form = () => {
-    const [adjective, setAdjective] = useState()
+    const [adjective, setAdjective] = useState('')
+
+    const [story, setStory] = useState('')
+    const [isVisible, setIsVisible] = useState(false)
 
     const handleAdjectiveChange =(e) => {
         e.preventDefault()
         setAdjective(e.target.value)
     }
 
+    const handleSubmit =(e) => {
+        e.preventDefault()
+        setIsVisible(true)
+        setStory(`There once was a boy with a ${adjective} balloon`)
+    }
     return ( 
         <>
     <form noValidate autoComplete="off">
       <TextField label="Adjective" variant="filled" name="adjective"  value={adjective} onChange={handleAdjectiveChange} />
+      <Buttons text='Generate Story' variant="contained" color="primary" handleClick={handleSubmit} />
     </form>
+    {isVisible && <p>{story}</p>}
     </>
      );
 }
