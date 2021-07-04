0a25cefdcdcbe2d5a22497a1affe89ba5efecab5
Jon Smith <aardvarkjon94@yahoo.com>
2021-07-04 14:02:15 -0500

    Added text to the about page

--- pages/about.js
+++ pages/about.js
@@ -11,8 +11,9 @@ const About = () => {
             <title>Test Project | about</title>
         </Head>
         <div className={styles.container}>
-            <h1>About Me</h1>
+            <h1 className={styles.title}>About Me</h1>
             <Image src={headshot} width={250} height={200} alt="Jon Smith's headshot" />
+            <p>Test page using the image tag vs the traditional html image tag</p>
         </div>
         </>
      );
