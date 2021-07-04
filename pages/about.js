f90659a5455df3a2fcc9cd805ae26ecef4e4373e
Jon Smith <aardvarkjon94@yahoo.com>
2021-07-04 13:44:14 -0500

    Imported a photo, using image tag

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
-
+import headshot from '../public/Headshot.jpg'
+import Image from 'next/image'
 const About = () => {
     return ( 
         <>
@@ -7,10 +8,8 @@ const About = () => {
             <title>Test Project | about</title>
         </Head>
         <div>
-            <h1>About</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta minima laudantium velit accusamus, maxime illo quisquam distinctio at obcaecati recusandae cupiditate repellat non fugiat ipsa, commodi consequatur! In, ad doloremque.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda ducimus, adipisci esse suscipit dolores placeat ipsum, repellat ab quasi sint aspernatur accusamus recusandae iste tempora distinctio vero non minima! Eos.</p>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis rem iste dignissimos illum! Doloremque iusto cumque saepe laborum! Libero esse, quos similique quia facilis sunt molestiae qui nobis nostrum natus.</p>
+            <h1>About Me</h1>
+            <Image src={headshot} alt="Jon Smith's headshot" />
         </div>
         </>
      );
