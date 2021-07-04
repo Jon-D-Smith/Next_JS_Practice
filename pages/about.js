06350bb2075a24ba4370ef3c73ca3fdbb99042e3
Jon Smith <aardvarkjon94@yahoo.com>
2021-07-04 13:52:40 -0500

    Added styling page

--- pages/about.js
+++ pages/about.js
@@ -1,15 +1,18 @@
 import Head from 'next/head'
 import headshot from '../public/Headshot.jpg'
 import Image from 'next/image'
+import styles from '../styles/About.module.css'
 const About = () => {
+
+
     return ( 
         <>
         <Head>
             <title>Test Project | about</title>
         </Head>
-        <div>
+        <div className={styles.container}>
             <h1>About Me</h1>
-            <Image src={headshot} alt="Jon Smith's headshot" />
+            <Image src={headshot} width={250} height={200} alt="Jon Smith's headshot" />
         </div>
         </>
      );
